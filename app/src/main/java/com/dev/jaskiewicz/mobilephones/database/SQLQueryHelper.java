9355055fc2998abc69cf08735123fe25116043f1
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-03-13 09:20:36 +0100

    Database structure. Prepared sql query for create mobiles table and database helper ready to create database on the device.

---
+++ app/src/main/java/com/dev/jaskiewicz/mobilephones/database/SQLQueryHelper.java
@@ -0,0 +1,16 @@
+package com.dev.jaskiewicz.mobilephones.database;
+
+
+public class SQLQueryHelper {
+
+    private SQLQueryHelper() {
+        // Klasa ma mieć same metody statyczne, więc nie chcę żeby ktokolwiek tworzył jej obiekt
+    }
+
+    public static final String CREATE_TABLE_MOBILES = "CREATE TABLE " +
+            MobilesTable.TABLE_NAME + "( " +
+            MobilesTable.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            MobilesTable.COLUMN_PRODUCER + " TEXT NOT NULL, " +
+            MobilesTable.COLUMN_MODEL + " TEXT NOT NULL, " +
+            MobilesTable.COLUMN_ANDROID_VERSION + " REAL);";
+}
