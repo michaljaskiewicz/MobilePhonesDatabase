78a96019300b66c7b0f730994f0fb9315c428a54
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-03-14 09:30:25 +0100

    Open www in web browser

--- app/src/main/java/com/dev/jaskiewicz/mobilephones/ui/AddOrEditPhoneFragment.java
+++ app/src/main/java/com/dev/jaskiewicz/mobilephones/ui/AddOrEditPhoneFragment.java
@@ -1,6 +1,8 @@
 package com.dev.jaskiewicz.mobilephones.ui;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,15 +14,17 @@ import android.widget.Toast;
 import com.dev.jaskiewicz.mobilephones.R;
 import com.dev.jaskiewicz.mobilephones.ui.validation.InputValidator;
 
+import static android.content.Intent.ACTION_VIEW;
+
 public class AddOrEditPhoneFragment extends Fragment {
 // TODO czy adres www mamy zapisywac w bazie
     private static final boolean DO_NOT_ATTACH_TO_ROOT = false;
 
     private InputValidator inputValidator;
-    private EditText producer;
-    private EditText model;
-    private EditText androidVersion;
-    private EditText url;
+    private EditText producerEditText;
+    private EditText modelEditText;
+    private EditText androidVersionEditText;
+    private EditText urlEditText;
     private Button searchUrl;
     private Button cancel;
     private Button save;
@@ -39,17 +43,17 @@ public class AddOrEditPhoneFragment extends Fragment {
     }
 
     private void findAllViews() {
-        producer = (EditText) getActivity().findViewById(R.id.producer_edit_text);
-        model = (EditText) getActivity().findViewById(R.id.model_edit_text);
-        androidVersion = (EditText) getActivity().findViewById(R.id.android_version_edit_text);
-        url = (EditText) getActivity().findViewById(R.id.www_edit_text);
+        producerEditText = (EditText) getActivity().findViewById(R.id.producer_edit_text);
+        modelEditText = (EditText) getActivity().findViewById(R.id.model_edit_text);
+        androidVersionEditText = (EditText) getActivity().findViewById(R.id.android_version_edit_text);
+        urlEditText = (EditText) getActivity().findViewById(R.id.www_edit_text);
         searchUrl = (Button) getActivity().findViewById(R.id.search_url_button);
         cancel = (Button) getActivity().findViewById(R.id.cancel_button);
         save = (Button) getActivity().findViewById(R.id.save_button);
     }
 
     private void createInputValidator() {
-        inputValidator = new InputValidator(producer, model, androidVersion, url);
+        inputValidator = new InputValidator(producerEditText, modelEditText, androidVersionEditText, urlEditText);
     }
 
     private void setUpOnClickListener() {
@@ -68,7 +72,7 @@ public class AddOrEditPhoneFragment extends Fragment {
                         savePhone();
                         break;
                     case R.id.search_url_button:
-                        searchPhone();
+                        searchPhoneInWebBrowser();
                         break;
                     case R.id.cancel_button:
                         //TODO dowiedzieć się co ma robić ten przycisk
@@ -109,7 +113,21 @@ public class AddOrEditPhoneFragment extends Fragment {
         Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT).show();
     }
 
-    private void searchPhone() {
+    private void searchPhoneInWebBrowser() {
+        if (inputValidator.isUrlValid()) {
+            openInWebBrowser();
+        } else {
+            showShortMessageWith(getString(R.string.url_not_valid_message));
+        }
+    }
 
+    private void openInWebBrowser() {
+        Intent intent = new Intent(ACTION_VIEW, Uri.parse(prepareCorrectUrl()));
+        startActivity(intent);
+    }
+
+    private String prepareCorrectUrl() {
+        final String url = urlEditText.getText().toString();
+        return UrlMaker.buildCorrectUrlFrom(url);
     }
 }
