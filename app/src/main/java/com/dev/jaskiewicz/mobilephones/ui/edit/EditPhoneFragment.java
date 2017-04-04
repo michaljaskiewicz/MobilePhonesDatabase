91887e4ee36884c518e1411eeba7280ef911983e
Michał Jaśkiewicz <michal.grzegorz.jaskiewicz@gmail.com>
2017-04-04 14:05:55 +0200

    Retrieving phone from database using Loader.

--- app/src/main/java/com/dev/jaskiewicz/mobilephones/ui/edit/EditPhoneFragment.java
+++ app/src/main/java/com/dev/jaskiewicz/mobilephones/ui/edit/EditPhoneFragment.java
@@ -1,10 +1,15 @@
 package com.dev.jaskiewicz.mobilephones.ui.edit;
 
+import android.app.LoaderManager;
 import android.content.ContentUris;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.dev.jaskiewicz.mobilephones.R;
 import com.dev.jaskiewicz.mobilephones.data.model.MobilePhone;
 import com.dev.jaskiewicz.mobilephones.data.MobilesContract;
 import com.dev.jaskiewicz.mobilephones.ui.AddOrEditPhoneFragment;
@@ -16,30 +21,75 @@ import static com.dev.jaskiewicz.mobilephones.data.database.MobilesTable.COLUMN_
 import static com.dev.jaskiewicz.mobilephones.data.database.MobilesTable.COLUMN_WWW;
 import static com.dev.jaskiewicz.mobilephones.ui.mainWindow.MobilesWindow.ID_OF_MOBILE_PHONE_CHOSEN_FOR_UPDATE;
 
-public class EditPhoneFragment extends AddOrEditPhoneFragment {
+public class EditPhoneFragment extends AddOrEditPhoneFragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String EMPTY_STRING = "";
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        MobilePhone phoneToEdit = getMobilePhoneToEdit();
-        updateGUIWith(phoneToEdit);
+        useLoaderToGetMobilePhoneToEdit();
     }
 
-    private MobilePhone getMobilePhoneToEdit() {
-        MobilePhone phone = null;
+    private void useLoaderToGetMobilePhoneToEdit() {
+        initLoader();
+    }
+
+    private void initLoader() {
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    protected void savePhoneInDatabase() {
+        // TODO updatePhoneInDatabase();
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         long phoneId = getIdOfMobilePhoneToEdit();
-        Cursor retrievedData = queryForMobilePhoneData(phoneId);
-        if (containsOnlyOneMobilePhone(retrievedData)) {
-            phone = createMobilePhoneFrom(retrievedData);
-        }
-        return phone;
+        return new CursorLoader(
+                getActivity(),
+                prepareUriForMobilePhoneWith(phoneId),
+                null,
+                null,
+                null,
+                null);
     }
 
     private long getIdOfMobilePhoneToEdit() {
         return getArguments().getLong(ID_OF_MOBILE_PHONE_CHOSEN_FOR_UPDATE);
     }
 
-    private MobilePhone createMobilePhoneFrom(Cursor mobilePhoneData) {
+    private Uri prepareUriForMobilePhoneWith(long phoneId) {
+        return ContentUris.withAppendedId(MobilesContract.CONTENT_URI, phoneId);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor retrievedData) {
+        if (containsOnlyOneMobilePhone(retrievedData)) {
+            updateGUIWith(retrievedData);
+        } else {
+            showErrorMessage();
+        }
+    }
+
+    private boolean containsOnlyOneMobilePhone(Cursor cursor) {
+        return cursor != null && cursor.getCount() == 1;
+    }
+
+    private void updateGUIWith(Cursor retrievedData) {
+        updateGUIWithMobilePhone(extractMobilePhoneFrom(retrievedData));
+    }
+
+    private void updateGUIWithMobilePhone(MobilePhone phoneToEdit) {
+        assignProducer(phoneToEdit.getProducer());
+        assignModel(phoneToEdit.getModel());
+        assignAndroidVersion(phoneToEdit.getAndroidVersion());
+        assignUrl(phoneToEdit.getWWW());
+    }
+
+    private MobilePhone extractMobilePhoneFrom(Cursor mobilePhoneData) {
         // Przesuwam kursor na pierwszą pozycję w celu odczytania jedynego zwróconego wiersza danych telefonu
         mobilePhoneData.moveToFirst();
 
@@ -52,30 +102,22 @@ public class EditPhoneFragment extends AddOrEditPhoneFragment {
         return new MobilePhone(id, producer, model, androidVersion, www);
     }
 
-    private boolean containsOnlyOneMobilePhone(Cursor cursor) {
-        return cursor != null && cursor.getCount() == 1;
+    private void showErrorMessage() {
+        Toast.makeText(getActivity(), R.string.error_no_phone_to_edit, Toast.LENGTH_SHORT).show();
     }
 
-    private Cursor queryForMobilePhoneData(long phoneId) {
-        return getActivity().getContentResolver()
-                .query(prepareUriForMobilePhoneWith(phoneId), null, null, null, null);
-    }
-
-    private Uri prepareUriForMobilePhoneWith(long phoneId) {
-        return ContentUris.withAppendedId(MobilesContract.CONTENT_URI, phoneId);
-    }
-
-    private void updateGUIWith(MobilePhone phoneToEdit) {
-        assignProducer(phoneToEdit.getProducer());
-        assignModel(phoneToEdit.getModel());
-        assignAndroidVersion(phoneToEdit.getAndroidVersion());
-        assignUrl(phoneToEdit.getWWW());
-    }
-
+    /**
+     * Dokumentacja zaleca, aby w tej metodzie usunąć wszystkie referencje do danych pobranych przez loadera
+     */
     @Override
-    protected void savePhoneInDatabase() {
-        // TODO updatePhoneInDatabase();
+    public void onLoaderReset(Loader<Cursor> loader) {
+        removePreviouslyAssignedData();
     }
 
-
+    private void removePreviouslyAssignedData() {
+        assignProducer(EMPTY_STRING);
+        assignModel(EMPTY_STRING);
+        assignAndroidVersion(EMPTY_STRING);
+        assignUrl(EMPTY_STRING);
+    }
 }
